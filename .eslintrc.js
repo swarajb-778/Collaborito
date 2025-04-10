657fded4401b422c9b8e3ef542ae7f129bf21910
Swaraj Bangar <swarajbangar77@gmail.com>
2025-04-09 22:23:25 -0700

    Add ESLint configuration for CI/CD

---
+++ .eslintrc.js
@@ -0,0 +1,1 @@
+module.exports = { extends: ["eslint:recommended"], env: { browser: true, node: true, es6: true }, parserOptions: { ecmaVersion: 2020, sourceType: "module" }, rules: { "no-unused-vars": "warn" }, ignorePatterns: [ "supabase/functions/**/*", "oauth-server.js", "node_modules/**/*" ] };
