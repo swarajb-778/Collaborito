bb6100e10e050a5eaa437361981fcc1247754736
Swaraj Bangar <swarajbangar77@gmail.com>
2025-04-12 16:29:41 -0700

    Fix dependency issues and CI/CD pipeline

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,15 @@
 module.exports = {
   preset: 'jest-expo',
   transformIgnorePatterns: [
-    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)'
+    'node_modules/(?!(jest-)?react-native|@react-native|@react-native-community|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|@rneui/.*|moti/.*)'
   ],
   setupFiles: [
     "<rootDir>/jest.setup.js"
   ],
   moduleNameMapper: {
     "\\.svg$": "<rootDir>/__mocks__/svgMock.js"
+  },
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": "babel-jest"
   }
 }; 
